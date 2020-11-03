acb743050598b206c46f82cd4ae174bc1e9cad19
ruanpato <ruanpato@outlook.com>
2020-11-03 17:43:02 -0300

    Testing

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const log = require('./helpers/lib/log/log');
-require("dotenv").config();
 
 const router = require('./routes/routes');
 
