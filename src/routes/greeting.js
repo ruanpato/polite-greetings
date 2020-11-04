5759c169abcffedf9e0a0d35a0b3430e3984b97a
ruanpato <ruanpato@outlook.com>
2020-11-04 16:50:23 -0300

    Testing cache

--- src/routes/greeting.js
+++ src/routes/greeting.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const express = require('express');
 const greetingController = require('../controllers/greeting');
 
@@ -5,5 +6,6 @@ const router = express.Router();
 
 router.get('/text', greetingController.getGreeting);
 router.get('/image', greetingController.getGreetingSVG);
+router.get('/test', greetingController.getGreetingSVGTest);
 
 module.exports = router;
